f6850a338c6bde2514a6e5eaafc655a075a55419
Crowdbotics <team@crowdbotics.com>
2023-06-21 02:49:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dark-waterfall-41645",
+  url: "https://dark-waterfall-41645.botics.co",
+  api: "https://dark-waterfall-41645.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
